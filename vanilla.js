6e32f9f0948570123a525a7ee2ce7571da742e75
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-11-28 21:22:18 +0000

    arr array Function and Selector

--- vanilla.js
+++ vanilla.js
@@ -16,35 +16,45 @@ let arrayExamplesEventsJS= [
 ]
 
 let arrayExamplesFunctionJS=[
-    "var newDiv = document.createElement( 'div' );var newContent = document.createTextNode( 'Some appended text.' ); newDiv.appendChild( newContent );",
-    "object.addEventListener( 'click', myFunction );",
-    "object.addEventListener( 'dblclick', myFunction );",
-    "document.addEventListener( 'keydown', (event) => { const keyName = event.key;} ); ",
-    "object.addEventListener( 'hover', myFunction );",
-    "var x = document.getElementById( 'myText' ).value;",
-    "imagesLoaded( document.querySelector( '#container' ), function( instance ) { console.log('all images are loaded'); } );",
-    "var image = new Image(); image.onerror = function() {        console.error( 'Cannot load image' ); }",
-    "document.getElementById( 'myForm' ).submit();",
-    "variable = document.getElementById( 'select_id' ).value;",
-    "ejemplo11",
-    "$( '#check_id' ).val();",
-    "Ejemplo 13"
+    "var newDiv = document.createElement( 'div' );",
+   " var el = document.getElementById( 'div_id' ); el.remove();",
+    ";var newContent = document.createTextNode( 'Some appended text.' ); newDiv.appendChild( newContent )",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    
 ]
 
 let arrayExamplesSelectorJS=[
-    "document.body.onload = function() { }",
-    "object.addEventListener( 'click', myFunction );",
-    "object.addEventListener( 'dblclick', myFunction );",
-    "document.addEventListener( 'keydown', (event) => { const keyName = event.key;} ); ",
-    "object.addEventListener( 'hover', myFunction );",
-    "var x = document.getElementById( 'myText' ).value;",
-    "imagesLoaded( document.querySelector( '#container' ), function( instance ) { console.log('all images are loaded'); } );",
-    "var image = new Image(); image.onerror = function() {        console.error( 'Cannot load image' ); }",
-    "document.getElementById( 'myForm' ).submit();",
-    "variable = document.getElementById( 'select_id' ).value;",
-    "ejemplo11",
-    "$( '#check_id' ).val();",
-    "Ejemplo 13"
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    "ejemplo",
+    
 ]
 
 function getExampleEventJS(position){
